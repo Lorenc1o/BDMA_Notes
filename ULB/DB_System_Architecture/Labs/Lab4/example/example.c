1171fb9aeb2e4dde659ff218718725e0543ce337
Lorenc1o <joseantoniolorencio@gmail.com>
2022-11-06 12:35:06 +0100

    Data mining: finished decision trees and model validation

---
+++ ULB/DB_System_Architecture/Labs/Lab4/example/example.c
@@ -0,0 +1,37 @@
+/*
+ * example PostgreSQL input/output function for bigint
+ *
+ * To learn how to write code for PostgreSQL extensions:
+ * C-Language Functions (https://www.postgresql.org/docs/13/xfunc-c.html)
+ */
+/* Add required libraries here */
+
+#include <stdio.h>
+#include "postgres.h"
+
+
+PG_MODULE_MAGIC;
+
+/******************************************************************************
+ * example Type Definition
+ ******************************************************************************/
+
+
+/******************************************************************************
+ * Conversion functions: example <-> str
+ ******************************************************************************/
+
+/* Type your code here */
+
+/******************************************************************************
+ * Input/Output
+ ******************************************************************************/
+
+/* Type your code here */
+
+
+/******************************************************************************
+ * Type Casting
+ ******************************************************************************/
+
+/* Type your code here */
