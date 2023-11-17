64dec9d775321e13751be90df4ebf0feec961e31
Lorenc1o <joseantoniolorencio@gmail.com>
2023-11-17 19:48:34 +0100

    ML: finished regularization chapter

---
+++ Paris-Saclay/Visual-Analytics/Labs/tutorial-web-basics-js/script.js
@@ -0,0 +1,147 @@
+/*
+    Basic JavaScript Programming
+*/
+
+/* Print Hello world! to the console */
+
+
+/*
+    1. Variables
+*/
+
+/* Define a variable called “greeting” and assigning it the value of “Hello world” */
+
+
+
+/* What is your current age? */
+
+
+/* What is the current datetime */
+
+
+/* Print out these variables to the console */
+
+
+/* A variable can be “undefined” at the beginning. */
+/* “null” is a value that is assigned to a variable when the variable should represent no value. */
+
+
+
+
+/*
+    2. Operations
+*/
+
+/* Define two numbers and calculate something using plus (+), minus (-), multiply (*), divide (/) */
+
+
+
+
+
+/* Check if two numbers are equal using === */
+
+
+/* == and != allow you to compare two things that may not be of the same type. */
+
+
+
+/*
+    3. Functions
+*/
+
+/* Define a function called “multiply” to multiply two numbers */
+
+
+
+
+
+/* Calling a multiple function and print the result */
+
+
+
+/*
+    4. Objects and Methods
+*/
+
+/* You can assign key-value pairs as an JavaScript object */
+
+
+
+
+
+
+/* You can access the value in the object the dot (.) notation. */
+
+
+/* Objects can have other objects and functions inside of them. */
+
+
+
+
+
+
+
+
+
+
+/*
+    5. Arrays
+    Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+    and https://www.w3schools.com/js/js_arrays.asp
+*/
+
+/* Define an arrary of fruits */
+
+
+
+
+
+/* Replace an element */
+
+
+
+/* Add a new element */
+
+
+
+
+/* An array can store elements of any type. */
+
+
+
+/* pop: extracts the last element of the array and returns it. */
+
+
+
+
+/* push: append the element to the end of the array. */
+
+
+
+/* shift: extracts the first element of the array and returns it. */
+
+
+
+/* unshift: add the element to the beginning of the array. */
+
+
+
+/* Other methods: filter(), forEach(), map(), reduce(), sort(), slice(), splice(), etc. */
+
+/*
+    6. Loops
+*/
+
+/* The classic way to cycle through array items is using a for loop over each index in the array. */
+
+
+
+
+/* JavaScript provides a shorthand version of the above, to use when you don’t need the index (i). */
+
+
+
+
+/* JavaScript also provides another approach to doing a for loop. */
+
+
